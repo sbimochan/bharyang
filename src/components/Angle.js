bafebc88c2f0d83c135b642eeefa404372922b2f
Bimochan Shrestha <bmochan@gmail.com>
2020-09-02 23:20:05 +0545

    Support decimal numbers

--- src/components/Angle.js
+++ src/components/Angle.js
@@ -22,7 +22,7 @@ export default function Angle() {
 				},
 			},
 		}));
-	const { handleSubmit, register } = useForm();
+	const { handleSubmit, register, errors } = useForm();
 
   const onSubmit = (values) => {
 		const hypotenuse = getHypotenuse(values.perpendicular, values.base);
@@ -33,37 +33,40 @@ export default function Angle() {
   };
 	const classes = useStyles();
   
-  return (<Paper elevation={3} classes={{ root: 'pl-10' }}>
-    <form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
-      <TextField
-        type="number"
-        name="perpendicular"
-        label="Height"
-        inputRef={register({ required: true })}
-      />
-      <TextField
-        type="number"
-        name="base"
-        label="Width"
-        inputRef={register({ required: true })}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
-    <Typography variant="caption" display="block">
-      Supports any type of units such as inches, cm, meters, etc.
-    </Typography>
-    <Typography variant="caption" display="block">
-      Make sure both height and base are in same units.
-    </Typography>
-    <div className="result-section">
-      <Typography variant="h5">Result</Typography>
-      <Typography variant="subtitle1">Hypotenuse</Typography>
-      {hypotenuse.toFixed(2)}
-      <Typography variant="subtitle1">Angle of ladder</Typography>
-      &theta; {angle.toFixed(2)}
-    </div>
-  </Paper>
-  )
+  return (
+		<Paper elevation={3} classes={{ root: 'pl-10' }}>
+			<form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
+				<TextField
+					name="perpendicular"
+					label="Height"
+					inputRef={register({ required: true, pattern: /^(\d*\.)?\d+$/ })}
+					error={!!errors.perpendicular}
+					helperText={errors.perpendicular?.type === 'pattern' && 'Please enter a valid number.'}
+				/>
+				<TextField
+					name="base"
+					label="Width"
+					inputRef={register({ required: true, pattern: /^(\d*\.)?\d+$/ })}
+					error={!!errors.base}
+					helperText={errors.base?.type === 'pattern' && 'Please enter a valid number.'}
+				/>
+				<Button type="submit" variant="contained" color="primary">
+					Submit
+				</Button>
+			</form>
+			<Typography variant="caption" display="block">
+				Supports any type of units such as inches, cm, meters, etc.
+			</Typography>
+			<Typography variant="caption" display="block">
+				Make sure both height and base are in same units.
+			</Typography>
+			<div className="result-section">
+				<Typography variant="h5">Result</Typography>
+				<Typography variant="subtitle1">Hypotenuse</Typography>
+				{hypotenuse.toFixed(2)}
+				<Typography variant="subtitle1">Angle of ladder</Typography>
+				&theta; {angle.toFixed(2)}
+			</div>
+		</Paper>
+	);
 }
--- src/components/Base.js
+++ src/components/Base.js
@@ -22,7 +22,7 @@ export default function Angle() {
 				},
 			},
 		}));
-	const { handleSubmit, register } = useForm();
+	const { handleSubmit, register,errors } = useForm();
 
   const onSubmit = (values) => {
     const base = getBase(values.perpendicular, values.angle);
@@ -32,37 +32,40 @@ export default function Angle() {
   };
 	const classes = useStyles();
   
-  return (<Paper elevation={3} classes={{ root: 'pl-10' }}>
-    <form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
-      <TextField
-        type="number"
-        name="perpendicular"
-        label="Height"
-        inputRef={register({ required: true })}
-      />
-      <TextField
-        type="number"
-        name="angle"
-        label="Angle in degree"
-        inputRef={register({ required: true })}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
-    <Typography variant="caption" display="block">
-      Supports any type of units such as inches, cm, meters, etc.
-    </Typography>
-    <Typography variant="caption" display="block">
-      Make sure both height and base are in same units.
-    </Typography>
-    <div className="result-section">
-      <Typography variant="h5">Result</Typography>
-      <Typography variant="subtitle1">Hypotenuse</Typography>
-      {hypotenuse.toFixed(2)}
-      <Typography variant="subtitle1">Required Width</Typography>
-      {base.toFixed(2)}
-    </div>
-  </Paper>
-  )
+  return (
+		<Paper elevation={3} classes={{ root: 'pl-10' }}>
+			<form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
+				<TextField
+					name="perpendicular"
+					label="Height"
+					inputRef={register({ required: true, pattern: /^(\d*\.)?\d+$/ })}
+					error={!!errors.perpendicular}
+					helperText={errors.perpendicular?.type === 'pattern' && 'Please enter a valid number.'}
+				/>
+				<TextField
+					name="angle"
+					label="Angle in degree"
+					inputRef={register({ required: true, pattern: /^(\d*\.)?\d+$/ })}
+					error={!!errors.angle}
+					helperText={errors.angle?.type === 'pattern' && 'Please enter a valid number.'}
+				/>
+				<Button type="submit" variant="contained" color="primary">
+					Submit
+				</Button>
+			</form>
+			<Typography variant="caption" display="block">
+				Supports any type of units such as inches, cm, meters, etc.
+			</Typography>
+			<Typography variant="caption" display="block">
+				Make sure both height and base are in same units.
+			</Typography>
+			<div className="result-section">
+				<Typography variant="h5">Result</Typography>
+				<Typography variant="subtitle1">Hypotenuse</Typography>
+				{hypotenuse.toFixed(2)}
+				<Typography variant="subtitle1">Required Width</Typography>
+				{base.toFixed(2)}
+			</div>
+		</Paper>
+	);
 }
